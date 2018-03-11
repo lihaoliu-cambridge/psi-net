e1bd1ede197198ed6e4614afb5b550e3ca003fe5
William <lhliu1994@gmail.com>
2018-03-11 16:51:14 +0800

    Add logs

---
+++ postprocessing/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .original_model import ColorNet
+from .unet import UNet
+from torchvision.models import resnet
